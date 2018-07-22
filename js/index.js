2930c5031c0333635b506abf5c7ed63f8f8ac268
Aniket-Batabyal <Aniketbatabyal1996@gmail.com>
2018-07-22 12:09:17 +0530

    added press events

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
 $("li").on("click",function(){
   $(this).toggleClass("completed")
 })
+
+//click on x to elete todolist
+
+$('span').on('click',function(event){
+  $(this).parent().fadeOut(500,function(){ //fadeOut on li
+    $(this).remove(); //all removed
+  });
+  event.stopPropagation();
+})
+$("input[type='text']").on("keypress(13)",function(){
+  alert('hey')
+})
