2b6d0df7fbf6e5207c8f3880678ca97bb9124bda
Aniket-Batabyal <Aniketbatabyal1996@gmail.com>
2018-07-28 14:45:12 +0530

    added add new todo features

--- js/index.js
+++ js/index.js
@@ -1,19 +1,25 @@
-$("li").on("click",function(){
+$("ul").on("click","li",function(){ // any time the ul is added , and the li is clicked inside this ul , only for the li clicked
   $(this).toggleClass("completed")
 })
 
 //click on x to elete todolist
 
-$('span').on('click',function(event){
+$('ul').on('click','span',function(event){
   $(this).parent().fadeOut(500,function(){ //fadeOut on li
     $(this).remove(); //all removed
   });
   event.stopPropagation();
 })
+$("input[type='text']").on('keypress',function(enter_key){
 
-var input = document.querySelectorAll("input[type='text'].textContent()");
-$("#btn1").on("click",function(){
-  $('ul').append(input.textContent());
+
+  if(enter_key.which === 13)
+  {
+    var text = $(this).val(); // extract the text
+    $(this).val("");
+    $('ul').append("<li><span>X</span> "+text+"</li>"); // this dont work when added new li's
+  }
+
 })
 /* to append , first we have to get the text from the textbox */
 /* how to ? */
